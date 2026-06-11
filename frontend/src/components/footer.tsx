977d72d9caf246dc443e364ec0fb248f8e0ce9d5
Delta <salmonzik01@gmail.com>
2026-06-11 08:39:16 +0200

    fix: update source repo link

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,15 +1,15 @@
 export function Footer() {
-	return (
-		<footer className="w-full text-center py-6 mt-auto text-xs text-zinc-500 dark:text-zinc-600 z-10 flex flex-row items-center justify-center gap-1.5">
-			<span>Исходный код этого сайта открыт —</span>
-			<a
-				href="https://github.com/0xd3174/course-work"
-				target="_blank"
-				rel="noopener noreferrer"
-				className="text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 transition-colors duration-200 underline decoration-zinc-300 dark:decoration-zinc-700 underline-offset-2 flex items-center gap-1"
-			>
-				GitHub
-			</a>
-		</footer>
-	);
+  return (
+    <footer className="w-full text-center py-6 mt-auto text-xs text-zinc-500 dark:text-zinc-600 z-10 flex flex-row items-center justify-center gap-1.5">
+      <span>Исходный код этого сайта открыт —</span>
+      <a
+        href="https://github.com/0xd3174/unfuck-elforun"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-zinc-700 dark:text-zinc-400 hover:text-zinc-900 dark:hover:text-zinc-200 transition-colors duration-200 underline decoration-zinc-300 dark:decoration-zinc-700 underline-offset-2 flex items-center gap-1"
+      >
+        GitHub
+      </a>
+    </footer>
+  );
 }
